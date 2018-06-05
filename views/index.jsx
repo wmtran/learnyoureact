b46ce061b630487d6c6a67149149268a1973e953
William Tran <mwtran@gmail.com>
2018-06-06 09:15:52 +1000

    completed exercise 3

--- views/index.jsx
+++ views/index.jsx
@@ -16,10 +16,15 @@ class TodoList extends React.Component {
     render() {
         return (
             <div className="todoList">
-                I am a TodoList.
+                <table style={{border: "2px solid black;"}}>
+                    <tbody>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>               
         );
-    }    
+    }
 }
 
 class TodoForm extends React.Component {
@@ -29,6 +34,17 @@ class TodoForm extends React.Component {
                 I am a TodoForm.
             </div>
 
+        );
+    }
+}
+
+class Todo extends React.Component {
+    render() {
+        return (
+        <tr>
+            <td style={{border: "1px solid black;"}}>{this.props.title}</td>
+            <td style={{border: "1px solid black;"}}>{this.props.children}</td>
+        </tr>
         );
     }
 }
